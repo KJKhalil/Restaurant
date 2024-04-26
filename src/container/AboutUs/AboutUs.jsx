51c7d5b049408c4444b093ed10c606a8a641453d
Khalil <khalil.j.khalil2@gmail.com>
2024-04-26 16:39:10 -0400

    added the knife image and G image to the section

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -1,3 +1,4 @@
+/*The About Button On The Navbar Will Take You To This Section*/
 import React from 'react';
 import { images } from '../../constants';
 import './AboutUs.css';
@@ -6,7 +7,7 @@ const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
 
     <div className='app__aboutus-overlay flex__center'>
-
+      <img src={images.G} alt='G Overlay'/>
     </div>
 
     <div className='app__aboutus-content flex__center'>
@@ -14,11 +15,11 @@ const AboutUs = () => (
     </div>
 
     <div className='app__aboutus-content__knife flex__center'>
-
+      <img src={images.knife} alt='About Knife'/>
     </div>
 
     <div className='app__aboutus-content__history'>
-      
+
     </div>
   </div>
 );
