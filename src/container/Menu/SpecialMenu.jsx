7d37db60e856d236ce351adfaf4070b294779019
Khalil <khalil.j.khalil2@gmail.com>
2024-04-29 12:53:00 -0400

    Started SpecialMenu.jsx. Made 2 import lines. Created started divs for tite,menu,wine,img,cocktails, and created a button for the rest of the menu

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-
+import { SubHeading, MenuItem } from '../../components';
+import { data, images } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    
+    <div className='app__specialMenu-title'>
+    </div>
+
+    <div className='app__specialMenu-menu'>
+
+      <div className='app__specialMenu-menu__wine flex__center'>
+      </div>
+
+      <div className='app__specialMenu-menu__img'>
+      </div>
+
+      <div className='app__specialMenu-menu__cocktails flex__center'>
+      </div>
+
+    </div> 
+     
+    <div style={{ marginTop: 15}}>
+      <button type='button' className='custom__button'>View More</button>
+    </div>
   </div>
 );
 
