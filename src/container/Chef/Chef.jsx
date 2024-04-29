1e062ed9920efdade5092019544cbc82edec9394
Khalil <khalil.j.khalil2@gmail.com>
2024-04-29 14:45:55 -0400

    started Chef.jsx. Imported SubHeading and images. Made starter divs for different sectios

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+    </div>
+
+    <div className='app__wrapper_info'>
+
+      <div className='app__chef-content'>
+      </div>
+
+      <div className='app__chef-sign'>
+      </div>
+    </div>
   </div>
 );
 
