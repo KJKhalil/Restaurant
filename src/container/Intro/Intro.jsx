b2e794d11f5e2e2d03cdfab10f5c3545bd1d8a53
Khalil <khalil.j.khalil2@gmail.com>
2024-04-29 15:55:12 -0400

    Started Intro.jsx. Imported react-icons, meal from constants, made added consts to control the video later on. Made a div for the video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
-
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  return (
+    <div className='app__video'>
+    </div>
+
+  );
+};
 
 export default Intro;
