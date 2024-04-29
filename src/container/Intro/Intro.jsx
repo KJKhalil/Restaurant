1152fe43fa38e3927c42266b217de7beb6b8e5ff
Khalil <khalil.j.khalil2@gmail.com>
2024-04-29 16:53:07 -0400

    Finished Intro.jsx. Added const handleVideo. Put In The Video, Put In The Play/Pause Button For The Video

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,3 +1,4 @@
+/*This Page Handles The Section Under The Chefs Section Displaying A Video */
 import React from 'react';
 import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
 import { meal } from '../../constants';
@@ -5,13 +6,44 @@ import './Intro.css';
 
 const Intro = () => {
 
+  /*Uses React To Control The Functionality Of The Video*/
   const [playVideo, setPlayVideo] = React.useState(false);
   const vidRef = React.useRef();
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo)
+    if(playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  }
 
   return (
     <div className='app__video'>
-    </div>
 
+      {/*Handles The Input Of The Video "meal"*/}
+      <video
+        ref={vidRef}
+        src={meal}
+        type='video/mp4'
+        loop
+        controls={false}
+        muted
+      />
+
+      <div className='app__video-overlay flex__center'>
+        
+        {/*Handles The Play/Pause Button For The Video "meal" Using const handleVideo*/}
+        <div 
+          className='app__video-overlay__circle flex_center' 
+          onClick={handleVideo}
+        >
+          {playVideo
+            ? <BsPauseFill color='#FFF' fontSize={30}/>
+            : <BsFillPlayFill color='#FFF' fontSize={30}/>}
+        </div>
+      </div>
+    </div>
   );
 };
 
